1aeccce3b8b02e1fcd222320918a5b51558e46c9
Jon Freer <j.freer@hotmail.co.uk>
2020-11-18 14:47:41 +0000

    added single channel rendering

--- PLY_Render/PLY_Render.cpp
+++ PLY_Render/PLY_Render.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <chrono>
-
+#include <cmath>
 #include <glm/vec3.hpp> // glm::vec3
 #include <glm/vec4.hpp> // glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
@@ -18,6 +18,7 @@
 #include<vector>
 #include<png.h>
 #include <errno.h>
+#include <map>
 
 
 #ifdef __APPLE__
@@ -36,19 +37,42 @@ std::vector<std::array<unsigned char, 3>> vCol;
 static GLubyte* pixels = NULL;
 
 //Image Index
-int imageIndex = 2;
+int imageIndex = 1362;
 int passIndex = 0;
+int frameCount = 0;
+
 bool loaded = false; //allow render to load for 1 iteration
 //Text Parser
-const int set_size = 100;
-float QW[set_size];
-float QX[set_size];
-float QY[set_size];
-float QZ[set_size];
-float TX[set_size];
-float TY[set_size];
-float TZ[set_size];
-string names[set_size];
+const int set_size = 1363;
+//float QW[set_size];
+//float QX[set_size];
+//float QY[set_size];
+//float QZ[set_size];
+//float TX[set_size];
+//float TY[set_size];
+//float TZ[set_size];
+//string names[set_size];
+
+struct Image {
+	string name;
+	int cameraID;
+	float QW;
+	float QX;
+	float QY;
+	float QZ;
+	float TX;
+	float TY;
+	float TZ;
+};
+
+struct Camera {
+	int height;
+	int width;
+	float focalY;
+};
+
+std::map<int, Camera> cameras;
+Image images[set_size];
 
 //String helper methods 
 template <typename Out>
@@ -75,6 +99,9 @@ static void screenshot_png(const char* filename, unsigned int width, unsigned in
 	GLubyte** pixels, png_byte** png_bytes, png_byte*** png_rows) {
 	size_t i, nvals;
 	const size_t format_nchannels = 4;
+
+	
+	
 	err = fopen_s(&filepoint,filename, "wb");
 	nvals = format_nchannels * width * height;
 	*pixels = (GLubyte*)realloc(*pixels, nvals * sizeof(GLubyte));
@@ -109,10 +136,124 @@ static void screenshot_png(const char* filename, unsigned int width, unsigned in
 	fclose(filepoint);
 }
 
+static void screenshot_png_grey(const char* filename, unsigned int width, unsigned int height,
+	GLubyte** pixels, png_byte** png_bytes, png_byte*** png_rows) {
+	size_t i, nvals;
+	const size_t format_nchannels = 1;
+
+
+
+	err = fopen_s(&filepoint, filename, "wb");
+	nvals = format_nchannels * width * height;
+	*pixels = (GLubyte*)realloc(*pixels, nvals *4* sizeof(GLubyte));
+	*png_bytes = (png_byte*)realloc(*png_bytes, nvals * sizeof(png_byte));
+	*png_rows = (png_byte**)realloc(*png_rows, height * sizeof(png_byte*));
+	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, *pixels);
+
+	for (i = 0; i < nvals; i++)
+		(*png_bytes)[i] = (*pixels)[i*4]; //just take the r value
+	for (i = 0; i < height; i++)
+		(*png_rows)[height - i - 1] = &(*png_bytes)[i * width * format_nchannels];
+	png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png) abort();
+	png_infop info = png_create_info_struct(png);
+	if (!info) abort();
+	if (setjmp(png_jmpbuf(png))) abort();
+	png_init_io(png, filepoint);
+	png_set_IHDR(
+		png,
+		info,
+		width,
+		height,
+		8,
+		PNG_COLOR_TYPE_GRAY,
+		PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT,
+		PNG_FILTER_TYPE_DEFAULT
+	);
+	png_write_info(png, info);
+	png_write_image(png, *png_rows);
+	png_write_end(png, NULL);
+	png_destroy_write_struct(&png, &info);
+	fclose(filepoint);
+}
+
+static void screenshot_png_grey_old(const char* filename, unsigned int width, unsigned int height,
+	GLubyte** pixels, png_byte** png_bytes, png_byte*** png_rows) {
+	size_t i, nvals;
+	const size_t format_nchannels = 1;
+	
+
+
+	err = fopen_s(&filepoint, filename, "wb");
+	nvals = width * height ;
+	*pixels = (GLubyte*)realloc(*pixels, nvals * 4 * sizeof(GLubyte));
+	*png_bytes = (png_byte*)realloc(*png_bytes, nvals * sizeof(png_byte));
+	*png_rows = (png_byte**)realloc(*png_rows, height * sizeof(png_byte*));
+	glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, *pixels);
+	
+	for (size_t j = 0; j < nvals; j++) {
+		(*png_bytes)[j] = (*pixels)[j*3];
+	}
+	for (i = 0; i < height; i++)
+		(*png_rows)[height - i - 1] = &(*png_bytes)[i * width * format_nchannels];
+	png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png) abort();
+	png_infop info = png_create_info_struct(png);
+	if (!info) abort();
+	if (setjmp(png_jmpbuf(png))) abort();
+	png_init_io(png, filepoint);
+	png_set_IHDR(
+		png,
+		info,
+		width,
+		height,
+		8,
+		PNG_COLOR_TYPE_GRAY,
+		PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT,
+		PNG_FILTER_TYPE_DEFAULT
+	);
+	png_write_info(png, info);
+	png_write_image(png, *png_rows);
+	png_write_end(png, NULL);
+	png_destroy_write_struct(&png, &info);
+	fclose(filepoint);
+}
+
+void loadCameraTXT() {
+	ifstream inFile;
+	inFile.open("D:\\Downloads\\dense\\sparse\\cameras.txt");
+
+	if (!inFile) {
+		cerr << "Unable to open file datafile.txt";
+		exit(1);   // call system to stop
+	}
+
+	int count = 0;
+	for (std::string line; getline(inFile, line); )
+	{
+
+		if (line.find("OPENCV") != std::string::npos || line.find("PINHOLE") != std::string::npos) {
+
+			std::vector<std::string> x = split(line, ' ');
+			int id = atoi(x[0].c_str());
+			cameras[id] = Camera();
+			cameras[id].width = atoi(x[2].c_str());
+			cameras[id].height = atoi(x[3].c_str());
+			cameras[id].focalY = atof(x[5].c_str());
+			count++;
+		}
+	}
+
+
+
+}
+
 
 void loadImageTXT() {
 	ifstream inFile;
-	inFile.open("D:\\Downloads\\gerrard-hall\\gerrard-hall\\sparse\\images.txt");
+	inFile.open("D:\\Downloads\\dense\\sparse\\images.txt");
 
 	if (!inFile) {
 		cerr << "Unable to open file datafile.txt";
@@ -124,17 +265,19 @@ void loadImageTXT() {
 	for (std::string line; getline(inFile, line); )
 	{
 
-		if (line.find("JPG") != std::string::npos) {
+		if (line.find("jpg") != std::string::npos || line.find("JPG") != std::string::npos) {
 
 			std::vector<std::string> x = split(line, ' ');
-			QW[count] = atof(x[1].c_str());
-			QX[count] = atof(x[2].c_str());
-			QY[count] = atof(x[3].c_str());
-			QZ[count] = atof(x[4].c_str());
-			TX[count] = atof(x[5].c_str());
-			TY[count] = atof(x[6].c_str());
-			TZ[count] = atof(x[7].c_str());
-			names[count] = x[9];
+			images[count] = Image();
+			images[count].QW = atof(x[1].c_str());
+			images[count].QX = atof(x[2].c_str());
+			images[count].QY = atof(x[3].c_str());
+			images[count].QZ = atof(x[4].c_str());
+			images[count].TX = atof(x[5].c_str());
+			images[count].TY = atof(x[6].c_str());
+			images[count].TZ = atof(x[7].c_str());
+			images[count].cameraID = atoi(x[8].c_str());
+			images[count].name = x[9];
 			count++;
 		}
 	}
@@ -155,11 +298,14 @@ void changeSize(int w, int h) {
 
 	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-
+	glutReshapeWindow(w, h);
 	//gluOrtho(0.0f, w, h, 0.0f, 0.0f, 1.0f);
 	//gluOrtho2D(0.0, 900.0, 0.0, 640.0);
 	// Set the correct perspective.
-	gluPerspective(52.01f, ratio, 0.1f, 100.0f);
+	float temp = (h / 2) / cameras[images[imageIndex].cameraID].focalY;
+	float fov = 2*(atan(temp) * 180 / 3.1415);
+	
+	gluPerspective(fov, ratio, 0.1f, 100.0f);
 
 	// Get Back to the Modelview
 	glMatrixMode(GL_MODELVIEW);
@@ -168,7 +314,7 @@ void changeSize(int w, int h) {
 
 
 void drawPointCloud() {
-	glPointSize(2.0f);
+	glPointSize(cameras[images[imageIndex].cameraID].width/500);
 	glBegin(GL_POINTS);
 	for (std::vector<std::array<double, 3>>::size_type i = 0; i != vPos.size(); i++) {
 		glColor3f((float)vCol[i][0] / 256, (float)vCol[i][1] / 256, (float)vCol[i][2] / 256);
@@ -184,7 +330,7 @@ void drawPointCloudDepth(glm::vec3 camPos) {
 	for (std::vector<std::array<double, 3>>::size_type i = 0; i != vPos.size(); i++) {
 		float distance = (float)glm::distance(camPos, glm::vec3(vPos[i][0], vPos[i][1], vPos[i][2]));
 
-		glColor3f(1-(distance / 6), 1-(distance / 6), 1-(distance / 6));
+		glColor3f((distance / 15), (distance / 15), (distance / 15));
 		glVertex3f(vPos[i][0], vPos[i][1], vPos[i][2]);
 	}
 	glEnd();
@@ -193,12 +339,19 @@ void drawPointCloudDepth(glm::vec3 camPos) {
 
 
 void renderScene(void) {
+	
+	int height = cameras[images[imageIndex].cameraID].height;
+	int width = cameras[images[imageIndex].cameraID].width;
+	if (frameCount == 0) {
+		changeSize(width, height);
+	}
+	
 	//set background color
 	if (passIndex == 0) {
 		glClearColor(1, 1, 1, 1);
 	}
 	else {
-		glClearColor(0, 0, 0, 1);
+		glClearColor(1, 1, 1, 1);
 	}
 
 	// Clear Color and Depth Buffers
@@ -208,10 +361,10 @@ void renderScene(void) {
 	// Set the camera
 
 
-	glm::quat orientation = glm::quat(QW[imageIndex], QX[imageIndex], QY[imageIndex], QZ[imageIndex]);
+	glm::quat orientation = glm::quat(images[imageIndex].QW, images[imageIndex].QX, images[imageIndex].QY, images[imageIndex].QZ);
 	glm::mat3 RotationMatrix = glm::toMat3(orientation);
 	glm::mat3 Inverse = glm::inverse(RotationMatrix);
-	glm::vec3 position = glm::vec3(TX[imageIndex], TY[imageIndex], TZ[imageIndex]);
+	glm::vec3 position = glm::vec3(images[imageIndex].TX, images[imageIndex].TY, images[imageIndex].TZ);
 
 	glm::vec3 newPos = -Inverse * position;
 
@@ -228,21 +381,27 @@ void renderScene(void) {
 	std::string name;
 	if (passIndex == 0) {
 		drawPointCloud();
-		name = "D:\\nerual\\output\\color\\" + names[imageIndex].substr(4, names[imageIndex].size() - 8) + "_color.png";
+		name = "D:\\nerual\\output2\\color\\" + images[imageIndex].name.substr(0, images[imageIndex].name.size() - 4) + ".png";
 	}
 	else {
 		drawPointCloudDepth(newPos);
-		name = "D:\\nerual\\output\\depth\\" + names[imageIndex].substr(4, names[imageIndex].size() - 8) + "_depth.png";
+		name = "D:\\nerual\\output2\\depth\\" + images[imageIndex].name.substr(0, images[imageIndex].name.size() - 4) + ".png";
 	}
 	
 	glutSwapBuffers();
-	
+	if (frameCount == 3||frameCount == 4) {
 
-	
-	
-	screenshot_png(name.c_str(), width, height, &pixels, &png_bytes, &png_rows);
 
-	if (loaded) {
+		if (passIndex == 0) {
+			screenshot_png(name.c_str(), width, height, &pixels, &png_bytes, &png_rows);
+		}
+		else {
+			screenshot_png_grey(name.c_str(), width, height, &pixels, &png_bytes, &png_rows);
+		}
+	}
+	
+	
+	if (frameCount == 5) {
 		if (imageIndex < set_size - 1) {
 			imageIndex++;
 		}
@@ -250,11 +409,25 @@ void renderScene(void) {
 			passIndex = 1;
 			imageIndex = 0;
 		}
-		loaded = false;
+		frameCount = 0;
 	}
 	else {
-		loaded = true;
+		frameCount++;
 	}
+
+	//if (loaded) {
+	//	if (imageIndex < set_size - 1) {
+	//		imageIndex++;
+	//	}
+	//	else if (passIndex == 0) {
+	//		passIndex = 1;
+	//		imageIndex = 0;
+	//	}
+	//	loaded = false;
+	//}
+	//else {
+	//	loaded = true;
+	//}
 	
 	
 }
@@ -265,9 +438,10 @@ void renderScene(void) {
 int main(int argc, char** argv) {
 
 	loadImageTXT();
-	cout << names[imageIndex];
+	loadCameraTXT();
+	//cout << names[imageIndex];
 	//load point cloud file
-	happly::PLYData plyIn("D:/nerual/fused.ply");
+	happly::PLYData plyIn("D:/Downloads/fused_backup.ply");
 	vPos = plyIn.getVertexPositions();
 	vCol = plyIn.getVertexColors();
 
@@ -275,7 +449,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(width, height);
+	glutInitWindowSize(100, 100);
 	glutCreateWindow("Fluid");
 
 
@@ -288,7 +462,7 @@ int main(int argc, char** argv) {
 
 	// register callbacks
 	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
+	//glutReshapeFunc(changeSize);
 	glutIdleFunc(renderScene);
 
 	glutIgnoreKeyRepeat(1);
